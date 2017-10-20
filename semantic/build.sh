42f0f4e6baf959936a5d152c856d21999edc1897
Valentin LAMBERT <valentin1.lambert@epita.fr>
2017-10-20 11:27:17 +0200

    [Init] Change the initiation way

---
+++ semantic/build.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+npm i webpack -S
+npm i react-router-dom webpack react-dom react style-loader css-loader babel-core babel-loader babel-preset-es2015 babel-preset-react -S
+npm install --save-dev babel-preset-stage-2
+npm install semantic-ui-react -S
